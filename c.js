f3bbcc087ae30405e623863ccdd4905f08b92c34
Th4t3g0 <113315881+Th4t3g0@users.noreply.github.com>
2022-11-20 16:49:15 +0200

    Update c.js

--- c.js
+++ c.js
@@ -1,102 +1,120 @@
-class Pawn {
-  constructor(position, movable,active) {
+
+class Piece {
+  constructor(name,position, movable,active) {
     this.position = position;
     this.movable = movable;
     this.active =active;
+    this.name=name;
   }
-take(){
-return;
-}
-move(){
-return;
-}
 
 }
-//
-class Castle {
-  constructor(position, movable,active) {
-    this.position = position;
-    this.movable = movable;
-    this.active =active;
-  }
-take(){
-console.log("Hello world");
-}
-move(){
-console.log("Hello world");
-}
+const chess_board= [[6,5,4,10,8,4,5,6],
+                  [1,1,1,1,1,1,1,1],
+                  [0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0],
+                  [-1,-1,-1,-1,-1,-1,-1,-1],
+                  [-6,-5,-4,-10,-8,-4,-5,-6]
+                  ];
 
-}
-//
-class Bishop {
-  constructor(position, movable,active) {
-    this.position = position;
-    this.movable = movable;
-    this.active =active;
-  }
-take(){
-console.log("Hello world");
-}
-move(){
-console.log("Hello world");
-}
+//var white_pawn = new Piece("pawn","a3",true,true);
 
-}
-//
-class Horse {
-  constructor(position, movable,active) {
-    this.position = position;
-    this.movable = movable;
-    this.active =active;
-  }
-take(){
-console.log("Hello world");
-}
-move(){
-console.log("Hello world");
-}
+var piece=[];
+var piece_before=[];
 
-}
-//
-class Queen {
-  constructor(position, movable,active) {
-    this.position = position;
-    this.movable = movable;
-    this.active =active;
-  }
-take(){
-console.log("Hello world");
-}
-move(){
-console.log("Hello world");
-}
+var er;
 
-}
-//
-class King {
-  constructor(position, movable,active,checked) {
-    this.position = position;
-    this.movable = movable;
-    this.active =active;
-    this.checked=checked;
-
-  }
-take(){
-console.log("Hello world");
-}
-move(){
-console.log("Hello world");
-}
-
-}
 /////////////
-let a=document.getElementById("p1");
-a.addEventListener("click",function(){
-document.getElementById("empy").innerHTML=a.innerHTML;
-a.innerHTML="";
-});
+//let a=document.getElementById("p1");
+//a.addEventListener("click",function(){
+//document.getElementById("empy").innerHTML=a.innerHTML;
+//a.innerHTML="";
+//});
+
+/////////////////////
+let all=document.querySelectorAll("button");
 
 
+
+
+for (let i = 0; i < all.length; i++) {
+  all[i].addEventListener("click",function(){
+
+
+
+
+
+    
+  //console.log(all[i].getElementsByTagName("img")[0].src);
+  //console.log(all[i].getElementsByTagName("img")[0].getAttribute("alt"));
+
+try {
+ er =all[i].getElementsByTagName("img")[0].getAttribute("alt");
+} catch (error) {
+  er=false;
+}
+
+
+    
+    if(er){
+      //console.log("hello world");
+      //piece.push(all[i].getElementsByTagName("img")[0]);
+      //console.log(all[i].getElementsByTagName("img")[0]);
+      if(piece.length>0){
+        piece=[];
+      piece_before=[];
+      
+      }
+      piece.push(all[i].innerHTML);
+      piece_before.push(i);
+      //console.log(all[i].innerHTML);
+      
+      
+      
+
+
+
+
+
+
+      
+    }
+    else if(!er && !piece.length==0){
+      //console.log("hello World");
+      all[i].innerHTML=piece[0];
+      all[piece_before].innerHTML="";
+      piece=[];
+      piece_before=[];
+      
+      
+      //all[i].createElement("img").src=piece[0].getElementsByTagName("img")[0].getAttribute("src");
+    }
+
+
+
+
+
+
+
+
+
+
+    
+});}
+/////////////////
+
+
+
+
+
+function hightlight(button) {
+if (button.getElementsByTagName("img")[0].getAttribute("alt")=="pawn") {
+  //all[i].getElementsByTagName("img")[0].getAttribute("alt")
+}
+
+  
+}
 
 
 
