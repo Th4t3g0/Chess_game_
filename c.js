cec347b94a2aed54d72f4d6f8acd9b8852881579
Th4t3g0 <113315881+Th4t3g0@users.noreply.github.com>
2023-01-23 10:41:06 +0200

    update
    added methods for castle,bishop ,king and bishop, King and the horse movements ahas bugs

--- c.js
+++ c.js
@@ -8,11 +8,11 @@ class Piece {
   }
 
 }
-const chess_board= [[0,1,2,3,4,5,6,7],
+var chess_board= [[0,1,2,3,4,5,6,7],
                   [8,9,10,11,12,13,14,15],
                   [16,17,18,19,20,21,22,23],
                   [24,25,26,27,28,29,30,31],
-                  [32,33,34,35,36,37,37,39],
+                  [32,33,34,35,36,37,38,39],
                   [40,41,42,43,44,45,46,47],
                   [48,49,50,51,52,53,54,55],
                   [56,57,58,59,60,61,62,63]
@@ -20,8 +20,8 @@ const chess_board= [[0,1,2,3,4,5,6,7],
 
 //var white_pawn = new Piece("pawn","a3",true,true);
 var p=0;
-var b1=7;
-var b2=9;
+var b1=0;
+var b2=0;
 var piece=[];
 var piece_before=[];
 var horse=[6,17,10,15,-10,-15,-6,-17];
@@ -29,7 +29,7 @@ var h1=[0,8,16,24,32,40,48,56];
 var h2=[7,15,23,31,39,47,55,63];
 var b9=[9,18,27,36,45,54,63,72,81,90];
 var b7=[7,14,21,28,35,42,49,56,63,70];
-//var h11;
+var take=[]
 //var h222;
 //var b77;
 //var b99;
@@ -60,15 +60,7 @@ try {
 }
 
 
-
-
-    
-  //console.log(all[i].getElementsByTagName("img")[0].src);
-  //console.log(all[i].getElementsByTagName("img")[0].getAttribute("alt"));
     if(er){
-      //console.log("hello world");
-      //piece.push(all[i].getElementsByTagName("img")[0]);
-      //console.log(all[i].getElementsByTagName("img")[0]);
       if(piece.length>0){
         setter(all);
         piece=[];
@@ -79,65 +71,42 @@ try {
       piece_before.push(i);
       all[i].style.backgroundColor="yellow";
 //start pawn
-      pawnM(all,i,"white pawn",16,"+");
-      pawnM(all,i,"black Pawn",47,"-");
+pawnM(all,i,"white pawn",16,"+");
+pawnM(all,i,"black Pawn",47,"-");
 //end pawn
 //start horse
-      horsemo(all,i,"white horse");
-      horsemo(all,i,"black horse");
+horsemo(all,i,"white horse");
+horsemo(all,i,"black horse");
      
 //end horse
-      
-///queen
+     
+//start bishop
+bishopMoves(all,i,"white bishop");
+bishopMoves(all,i,"black bishop");
 
+//end bishop
+///queen
+QueenMoves(all,i,"white queen");
+QueenMoves(all,i,"black queen");
+      
 
 ///end queen
 
 //King
-
+kingsMove(all,i,"white king");
+kingsMove(all,i,"black king");
 
 
 //end King
       
 //Castle
-castleM(all,i,"white castle");
-castleM(all,i,"black castle");
-
-
-
+castleMoves(all,i,"black castle");
+castleMoves(all,i,"white castle");
 //end Castle
 
-
-
-//
-
-
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
   
-  //all[i].getElementsByTagName("img")[0].getAttribute("alt")
 }
 // end of the first click
-
-
-
-
-
-
 
 
 
@@ -152,9 +121,7 @@ castleM(all,i,"black castle");
       all[piece_before].innerHTML="";
       piece=[];
       piece_before=[];
-    }
-      
-      //all[i].createElement("img").src=piece[0].getElementsByTagName("img")[0].getAttribute("src");
+    } 
     }
     ///end of the second click
 
@@ -171,53 +138,6 @@ castleM(all,i,"black castle");
 });}
 /////////////////
 
-
-
-
-
-
-///function for diagonals for queen and bishop
-function diagonals() {
-  
-  
-  for (let iii = 0; iii < 7; iii++) {
-        try {
-        all[i+b1].style.borderColor="#eb8334";
-        all[i+b2].style.borderColor="#eb8334";
-        
-          if (!(b1+i==0 || b1+i==8 || b1+i==16 || b1+i==24 || b1+i==32 || b1+i==40 || b1+i==48 || b1+i==56 )) {
-           b1+=7;
-            // break;
-          }
-          if (!(b2+i==7 || b2+i==15 || b2+i==23 || b2+i==31 || b2+i==39 || b2+i==47 || b2+i==55 || b1+i==63 )) {
-            b2+=9;
-            //break;
-          }
-          
-        
-        }catch (error) {
-
-          break;
-        }
-        
-      }
-        b1=7;
-  
-  
-}
-
-
-
-
-
-
-///
-
-
-////button.getElementsByTagName("img")[0].getAttribute("alt")=="pawn"
-
-///methods
-
 ///method for highting pawn movements
 function pawnM(all,i,piece,end,sign) {
   let r;
@@ -228,7 +148,6 @@ r=i<end
     r=i>end
   }
 
-  
   if (all[i].getElementsByTagName("img")[0].getAttribute("alt")==piece && r) {
         
       for (let iii = 0; iii < 2; iii++) {
@@ -240,21 +159,11 @@ r=i<end
         p=8;
       }else if(all[i].getElementsByTagName("img")[0].getAttribute("alt")==piece){
        
-        //console.log("hello world");
+       
         all[eval(i+sign+p)].style.borderColor="#eb8334";
       }
   
 }
-
-////method for highllighting bishop movements
-
-
-
-
-
-
-
-
 
 
 //method for clearing out borders
@@ -265,6 +174,7 @@ for (let j = 0; j < button.length; j++) {
 }
   
 }
+
 ///horse method
 function horsemo(all,i,piece) {
    if (all[i].getElementsByTagName("img")[0].getAttribute("alt")==piece ) {
@@ -278,76 +188,168 @@ function horsemo(all,i,piece) {
   
 }
 
-function castleM(all,i,piece) {
 
-  if (all[i].getElementsByTagName("img")[0].getAttribute("alt")==piece) {
+function bishopMoves(all,i,name) {
+  if (all[i].getElementsByTagName("img")[0].getAttribute("alt")==name) {
+let list =BishopMatrix(i,chess_board);
+    for (let ww = 0; ww < list.length; ww++) {
+      all[list[ww]].style.borderColor="#eb8334";
+   
+    }
+  }
   
-      for (let iii = 0; iii < 8; iii++) {
+}
+
+
+//Castle method begins
+function castleMoves(all,i,name) {
+  if (all[i].getElementsByTagName("img")[0].getAttribute("alt")==name) {
+let list =castleMatrix(i,chess_board);
+    for (let ww = 0; ww < list.length; ww++) {
+      all[list[ww]].style.borderColor="#eb8334";
+    }
+  }
+}
+//
+function castleMatrix(i,board) {
+let row=Math.floor(i/8);
+let col=i%8;
+let mat=[];
+  for (let ii = 0; ii < 8; ii++) {
+    //console.log(ii);
+  mat.push(board[ii][col]);
+    //console.log(mat)
+   // console.log(board[row][ii]);
+  mat.push(board[row][ii]);
+    
+  
+  }
+  mat=mat.filter(function(item){return item !==i});
+  mat=mat.filter(function(element){return element !== undefined;});
+  return mat;
+  
+}
+//Castle method ends
+
+
+//Bishop method begins
+function BishopMatrix(i,board){
+let row=Math.floor(i/8);
+let col=i%8;
+let mat=[];
+let x=0;
+
+  for (let ii = 0; ii < 8; ii++) {
+    try {
+      mat.push(board[row+x][col+x]);
+      
+      x+=1;
+    } catch (error) {
+      break;
+    }   
+  }
+  x=0;
+
+  for (let ii = 0; ii < 8; ii++) {
+    try {
+      mat.push(board[row-x][col-x]);
+      x+=1;
+    } catch (error) {
+      break;
+    }   
+  }
+  x=0;
+
+  
+  for (let ii = 0; ii < 8; ii++) {
+    try {
+      mat.push(board[row+x][col-x]);
+      x+=1;
+    } catch (error) {
+      break;
+    }   
+  }
+  x=0;  
+  for (let ii = 0; ii < 8; ii++) {
+    try {
+      mat.push(board[row-x][col+x]);
+      x+=1;
+    } catch (error) {
+      break;
+    }   
+  }
+  x=0;
+  mat=mat.filter(function(item){return item !==i});
+  mat=mat.filter(function(element){return element !== undefined;});
+  return mat;
+}
+//Bishop ends
+
+//Queen movements
+function QueenMoves(all,i,name) {
+
+  if (all[i].getElementsByTagName("img")[0].getAttribute("alt")==name) {
+    let list=[];
+  list=BishopMatrix(i,chess_board);
+  list2=castleMatrix(i,chess_board);
+  for (let a of list2) {
+    list.push(a);
+  }
+  for (let ww = 0; ww < list.length; ww++) {
+      all[list[ww]].style.borderColor="#eb8334";
+    }
+  }
+}
+//King movements
+function kingsMove(all,i,name) {
+if (all[i].getElementsByTagName("img")[0].getAttribute("alt")==name) {
+let row=Math.floor(i/8);
+let col=i%8;
+let mat=[];
+let x=1,y=8,z=7,q=9;
+let ks=[1,8,7,9,-1,-8,-7,-9];
+  
+ for (let iii = 0; iii < 8; iii++) {
         try {
-          p+=8;
-        all[i+p].style.borderColor="#eb8334";
-          //console.log("working1a");
-        
+          all[i+ks[iii]].style.borderColor="#eb8334";
         } catch (error) {
-          
-          continue;}
-        
-      }
-        p=0;
-     for (let iii = 0; iii < 8; iii++) {
-       try {
-         p-=8;
-        all[i+p].style.borderColor="#eb8334";
-         
-        
-         } catch (error) {
-         
-         continue;
-                          }
-      }
-        p=0;
+          continue;
+        } } 
+}
+}
+
+function TakePawnW(all,i,name) {
+let row=Math.floor(i/8);
+let col=i%8;
+let mat=[];
+  if (all[i].getElementsByTagName("img")[0].getAttribute("alt")==name) {
+    try {
+      
+  mat.push(chess_board[row+1][col+1]);
+} catch (error) {
+  
+}
+try {
+  mat.push(chess_board[row+1][col-1]);
+} catch (error) {
+}
+
+  } else {
+    try {
+      
+  mat.push(chess_board[row-1][col+1]);
+} catch (error) {
+  
+}
+try {
+  mat.push(chess_board[row-1][col-1]);
+} catch (error) {
+}
 
     
-      
-   for (let iii = 0; iii < 8; iii++) {
-     if (ka) {
-       try {
-        all[i+one].style.borderColor="#eb8334";
-         for (let zz of h2) {
-           if (zz==(i+one)||zz==i) {
-             ka=false;
-           }}
-        
-         } catch (error) {continue;}
-   }
-     one+=1;
-      }
-        one=1;
-        ka=true;
-  for (let iii = 0; iii < 8; iii++) {
-    if (ka) {
-       try {
-        all[i-one].style.borderColor="#eb8334";
-        
-         for (let zz of h1) {
-           if (zz==(i-one)||zz==i) {
-             ka=false;
-           }}
-       
-         } catch (error) {continue;}
-}
-    one+=1;
-      }
-  ka=true;
-  one=1;
-}
+  }
+
+
+return mat;
 }
 
-function bishopM(all,i,name) {
-
-
-  
-}
-
-
-////
